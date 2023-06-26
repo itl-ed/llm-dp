7fa9747c27dfea1d5f8ec5674337b58c2eee6b62
Gautier Dagan <s2234411@ed.ac.uk>
2023-06-26 17:34:10 +0100

    simplify calls to LLM to use single call for goal generation

--- llmdp.py
+++ llmdp.py
@@ -5,7 +5,7 @@ import random
 import re
 import time
 import logging
-from collections import defaultdict
+from collections import defaultdict, Counter
 
 import openai
 import tiktoken
@@ -24,92 +24,40 @@ chat_encoding = tiktoken.encoding_for_model(LLMDPConfig.llm_chat_model)
 openai.api_key = LLMDPConfig.openai_api_key
 
 
-FIND_OBJECTS_PROMPT = """Given a task outline what object the planner needs to find/look for.
-Your task is to: cool some egg and put it in garbagecan.
-egg
-Your task is to: put a hot plate in fridge.
-plate
-Your task is to: clean some soapbar and put it in countertop.
-soapbar
-Your task is to: put a soapbottle in cabinet.
-soapbottle
-Your task is to: examine the cd with the desklamp.
-cd, desklamp
-Your task is to: put some toiletpaper on toiletpaperhanger.
-toiletpaper
-Your task is to: put two cellphone in bed.
-cellphone, cellphone"""
-
-FIND_RECEPTACLES_PROMPT = """Given a task outline what receptacles the planner needs to find/look (if any). If none, reply with 'none'.
-Your task is to: cool some egg and put it in garbagecan.
-garbagecan
-Your task is to: put a hot plate in fridge.
-fridge
-Your task is to: clean some soapbar and put it in countertop.
-countertop
-Your task is to: put a soapbottle in cabinet.
-cabinet
-Your task is to: examine the cd with the desklamp.
-none
-Your task is to: put some toiletpaper on toiletpaperhanger.
-toiletpaperhanger
-Your task is to: put two cellphone in bed.
-bed
-"""
-
 GENERATE_GOAL_PROMPT = """(define (domain alfred)
-(:requirements :typing)
-(:types
-receptacle object rtype
-)
-(:constants
-SinkBasinType - rtype
-MicrowaveType - rtype
-FridgeType - rtype
-)
 (:predicates
-(atReceptacleLocation ?r - receptacle) ; true if the robot is at the receptacle location
-(inReceptacle ?o - object ?r - receptacle) ; true if object ?o is in receptacle ?r
-(openable ?r - receptacle) ; true if a receptacle is openable
-(opened ?r - receptacle) ; true if a receptacle is opened
+(isReceptacle ?o - object) ; true if the object is a receptacle
+(atReceptacleLocation ?r - object) ; true if the robot is at the receptacle location
+(inReceptacle ?o - object ?r - object) ; true if object ?o is in receptacle ?r
+(openable ?r - object) ; true if a receptacle is openable
+(opened ?r - object) ; true if a receptacle is opened
 (isLight ?o - object) ; true if an object is light source
 (examined ?o - object ?l - object) ; whether the object has been looked at with light
 (holds ?o - object) ; object ?o is held by robot
 (isClean ?o - object) ; true if the object has been cleaned in sink
 (isHot ?o - object) ; true if the object has been heated up
 (isCool ?o - object) ; true if the object has been cooled
-(receptacleType ?r - receptacle ?t - rtype) ; true if the receptacle is of type ?t
+(isSink ?o - object) ; true if the object is a sink
+(isMicrowave ?o - object) ; true if the object is a microwave
+(isFridge ?o - object) ; true if the object is a fridge
 )
 )
 TASK: Your task is to: put a clean plate in microwave.
-(:objects
-microwave-1 receptacle
-plate-1 - object
-)
 (:goal
-(exists (?t - object)
-(and (inReceptacle ?t microwave-1)
+(exists (?t - plate ?r - microwave)
+(and (inReceptacle ?t ?r)
 (isClean ?t)
 )))
 TASK: Your task is to: examine an alarmclock with the desklamp
-(:objects
-alarmclock-1 - object
-desklamp-1 - object
-)
 (:goal
-(exists (?t - object ?l - object) 
-(examined ?t ?l)
-))
+(exists (?t - alarmclock ?l - desklamp)
+(and (examined ?t ?l) (holds ?t)
+)))
 TASK: Your task is to: put two cellphone in bed
-(:objects
-bed-1 receptacle
-cellphone-1 - object
-cellphone-2 - object
-)
 (:goal
-(exists (?t1 - object ?t2 - object)
-(and (inReceptacle ?t1 bed-1)
-(inReceptacle ?t2 bed-1)
+(exists (?t1 - cellphone ?t2 - cellphone ?r - bed)
+(and (inReceptacle ?t1 ?r)
+(inReceptacle ?t2 ?r)
 (not (= ?t1 ?t2))
 )))"""
 
@@ -232,6 +180,7 @@ class LLMDPAgent:
         env_id=0,
         logger=None,
         use_llm_search=False,
+        top_n=3,
     ) -> None:
         self.initial_scene_observation = initial_scene_observation
         self.task_description = task_description
@@ -240,17 +189,8 @@ class LLMDPAgent:
         self.llm_chat_tokens_sent = 0
         self.logger = logger
         self.use_llm_search = use_llm_search
+        self.top_n = top_n
 
-        # identify relevant objects and receptacle from task_description for goal
-        self.relevant_task_objects = self.get_relevant_task_objects(task_description)
-        # set of object types to look out for (e.g. apple, bread, etc.)
-        self.revevant_task_object_type = set(
-            [o.split("-")[0] for o in self.relevant_task_objects]
-        )
-        # identify relevant receptacles from task_description for goal
-        self.relevant_task_receptacles = self.get_relevant_task_receptacles(
-            task_description
-        )
         # get PDDL objects from scene_observation
         self.scene_receptacles = self.find_receptacles_from_scene_observation(
             initial_scene_observation
@@ -260,20 +200,21 @@ class LLMDPAgent:
         self.scene_objects = defaultdict(dict)
         for r in self.scene_receptacles:
             self.scene_objects[r] = self.get_receptacle_attributes(r)
-            self.scene_objects[r]["type"] = "receptacle"
-            self.scene_objects[r]["seen"] = False
-            if r in self.relevant_task_receptacles:
-                self.scene_objects[r]["relevant"] = True
-
-        for o in self.relevant_task_objects:
-            self.scene_objects[o]["type"] = "object"
-            self.scene_objects[o]["relevant"] = True
-            self.scene_objects[o]["seen"] = False
-            if "lamp" in o:
-                self.scene_objects[o]["isLight"] = True
 
         self.pddl_goal = self.get_pddl_goal()
         self.logger.info(f"PDDL GOAL: {self.pddl_goal}")
+
+        # Get objects that should exist based on generated PDDL goal
+        existential_pattern = r"\?\w+\s+-\s+(\w+)"
+        for o, count in Counter(
+            re.findall(existential_pattern, self.pddl_goal)
+        ).items():
+            for i in range(1, int(count) + 1):
+                name = f"{o}-{i}"
+                self.scene_objects[name]["type"] = o
+                self.scene_objects[name]["seen"] = False
+                if "lamp" in name:
+                    self.scene_objects[name]["isLight"] = True
 
         self.plan = self.get_plan()
 
@@ -288,7 +229,6 @@ class LLMDPAgent:
         """
         Given a list of objects, return a list of objects with a count id appended to each object.
         """
-
         count_dict = {}
         output_list = []
 
@@ -301,56 +241,14 @@ class LLMDPAgent:
 
         return output_list
 
-    def get_relevant_task_objects(self, task_description: str) -> list[str]:
-        """
-        Given a task description, return the object(s) that the planner needs to look for.
-        """
-        prompt = f"{FIND_OBJECTS_PROMPT}\n{task_description}\n"
-        task_objects_str = self.llm(prompt, stop=["\n"]).strip()
-        self.logger.info(task_objects_str)
-        task_objects = []
-        if "," in task_objects_str:
-            task_objects = task_objects_str.split(", ")
-        else:
-            task_objects = [task_objects_str]
-        # add -1, -2, -3, etc. to the end of each object
-        return self.get_object_count_id(task_objects)
-
-    def get_relevant_task_receptacles(self, task_description: str) -> list[str]:
-        """
-        Given a task description, return the receptacle that the planner needs to look at.
-        """
-        prompt = f"{FIND_RECEPTACLES_PROMPT}\n{task_description}\n"
-        task_receptacles_str = self.llm(prompt, stop=["\n"]).strip()
-        self.logger.info(task_receptacles_str)
-        if task_receptacles_str.strip() == "none":
-            return []
-
-        task_receptacles = []
-        if "," in task_receptacles_str:
-            task_receptacles = task_receptacles_str.split(", ")
-        else:
-            task_receptacles = [task_receptacles_str]
-        # add -1, -2, -3, etc. to the end of each receptacle
-        return self.get_object_count_id(task_receptacles)
-
     def get_task_goal(
         self,
         task_description: str,
-        relevant_objects: list[str],
-        relevant_receptacles: list[str],
     ) -> list[str]:
         """
         Given a task description, relevant objects/receptacles return the PDDL goal.
         """
-
-        objects_str = ""
-        for relevant_receptacle in relevant_receptacles:
-            objects_str += f"{relevant_receptacle} - receptacle\n"
-        for relevant_object in relevant_objects:
-            objects_str += f"{relevant_object} - object\n"
-
-        prompt = f"{GENERATE_GOAL_PROMPT}\nTASK: {task_description}\n(:objects\n{objects_str}\n)\n(goal:"
+        prompt = f"{GENERATE_GOAL_PROMPT}\nTASK: {task_description}\n(goal:"
         pddl_goal = self.llm(prompt, stop=None).strip()
         return pddl_goal
 
@@ -396,18 +294,18 @@ class LLMDPAgent:
 
     @staticmethod
     def get_receptacle_attributes(receptacle: str) -> dict:
+        """
+        Given a receptacle, return a dictionary of attributes.
+        """
         attributes = {}
 
         # predicate special types
         if "sink" in receptacle:
-            receptacleType = "SinkBasinType"
-            attributes["receptacleType"] = receptacleType
+            attributes["isSink"] = True
         elif "microwave" in receptacle:
-            receptacleType = "MicrowaveType"
-            attributes["receptacleType"] = receptacleType
+            attributes["isMicrowave"] = True
         elif "fridge" in receptacle:
-            receptacleType = "FridgeType"
-            attributes["receptacleType"] = receptacleType
+            attributes["isFridge"] = True
 
         # predicate openable
         if (
@@ -418,6 +316,10 @@ class LLMDPAgent:
             or "safe" in receptacle
         ):
             attributes["openable"] = True
+
+        attributes["type"] = receptacle.split("-")[0]
+        attributes["seen"] = False
+        attributes["isReceptacle"] = True
 
         return attributes
 
@@ -453,11 +355,7 @@ class LLMDPAgent:
 
     def get_pddl_goal(self) -> str:
         # use LLM to complete pddl goal
-        completed_pddl_goal = self.get_task_goal(
-            self.task_description,
-            self.relevant_task_objects,
-            self.relevant_task_receptacles,
-        )
+        completed_pddl_goal = self.get_task_goal(self.task_description)
         return f"(:goal {completed_pddl_goal}"
 
     def get_pddl_objects(self) -> str:
@@ -467,67 +365,75 @@ class LLMDPAgent:
         )
         return f"(:objects {objects_str})\n"
 
-    def get_pddl_init(self) -> str:
+    def get_pddl_init(self) -> list[str]:
         # fill in known predicates from observation
-        predicates = ""
+        fixed_predicates = ""
+        # fixed predicates
         for r, atts in self.scene_objects.items():
             for att, val in atts.items():
-                if att in ["type", "relevant", "seen"] or val is False:
+                if att in ["type", "seen"] or val is False:
                     continue
                 if val is True:
-                    predicates += f"({att} {r})\n"
+                    fixed_predicates += f"({att} {r})\n"
                 else:
-                    predicates += f"({att} {r} {val})\n"
+                    fixed_predicates += f"({att} {r} {val})\n"
 
+        # dynamic predicates (World Beliefs)
         unseen_receptacles = [
             r
             for r, atts in self.scene_objects.items()
-            if not atts["seen"] and atts["type"] == "receptacle"
+            if not atts["seen"] and "isReceptacle" in atts
         ]
         unseen_objects = [
             o
             for o, atts in self.scene_objects.items()
-            if not atts["seen"] and atts["type"] == "object"
+            if not atts["seen"] and "isReceptacle" not in atts
         ]
 
-        # if unseen objects, use LLM to guess receptacle
-        if len(unseen_objects) > 0:
-            # We can assume that the task is not to put an object in place that it already is
-            if len(unseen_receptacles) > 1:
-                unseen_receptacles = list(
-                    set(unseen_receptacles) - set(self.relevant_task_receptacles)
+        # if no unseen objects, return fixed predicates
+        if len(unseen_objects) == 0:
+            return [f"(:init {fixed_predicates})\n"]
+
+        belief_predicates = []
+        for _ in range(self.top_n):
+            # if no unseen receptacles
+            if len(unseen_receptacles) == 0:
+                break
+
+            belief_predicate = fixed_predicates
+            # Use LLM to guess which receptacle
+            if self.use_llm_search:
+                likely_receptacle, llm_tokens = llm_find_objects(
+                    unseen_objects, unseen_receptacles, self.logger
                 )
-
-            if len(unseen_receptacles) > 1:
-                # Use LLM to guess which receptacle
-                if self.use_llm_search:
-                    likely_receptacle, llm_tokens = llm_find_objects(
-                        unseen_objects, unseen_receptacles, self.logger
-                    )
-                    self.llm_chat_tokens_sent += llm_tokens
-                # Use a random unseen receptacle
-                else:
-                    likely_receptacle = random.choice(unseen_receptacles)
-
+                self.llm_chat_tokens_sent += llm_tokens
+            # Use a random unseen receptacle otherwise
             else:
-                # If there is only one unseen receptacle, use that
-                likely_receptacle = unseen_receptacles[0]
+                likely_receptacle = random.choice(unseen_receptacles)
 
+            # remove likely receptacle from unseen receptacles
+            unseen_receptacles.remove(likely_receptacle)
+
+            # guess that object in likely receptacle
             for o in unseen_objects:
-                predicates += f"(inReceptacle {o} {likely_receptacle})\n"
+                belief_predicate += f"(inReceptacle {o} {likely_receptacle})\n"
 
-        pddl_init = f"(:init {predicates})\n"
-        return pddl_init
+            belief_predicates.append(belief_predicate)
 
-    def get_pddl_problem(
-        self,
-    ) -> str:
-        # construct to PDDL problem.pddl
-        problem = (
-            "(define (problem alf)\n(:domain alfred)\n"
-            + f"{self.get_pddl_objects()}{self.get_pddl_init()}{self.pddl_goal})"
-        )
-        return problem
+        return [f"(:init {predicates})\n" for predicates in belief_predicates]
+
+    def get_pddl_problem(self) -> list[str]:
+        # get n different init configurations
+        inits = self.get_pddl_init()
+
+        problems = []
+        for init in inits:
+            # construct to PDDL problem.pddl
+            problems.append(
+                "(define (problem alf)\n(:domain alfred)\n"
+                + f"{self.get_pddl_objects()}{init}{self.pddl_goal})"
+            )
+        return problems
 
     def update_observation(self, observation: str) -> bool:
         if observation == "":
@@ -541,7 +447,7 @@ class LLMDPAgent:
         action_args = action_args[1:]
 
         # we use the last action to update the scene
-        # NOTE: this is using the symbolic :effect of the action
+        # NOTE: this is using the symbolic :effects of the action
         #       as described in the PDDL domain
         match action_name:
             case "examineobjectinlight":
@@ -590,25 +496,27 @@ class LLMDPAgent:
                     del self.scene_objects[obj]["inReceptacle"]
 
             # update inReceptacle for all objects observed at this receptacle
-            # NOTE: we ignore seen objects that are not relevant to the task
+            # TODO: Test without: NOTE: we ignore seen objects that are not relevant to the task
             # We also add objects of the same type as the target object
             for obj in objects:
                 if obj in self.scene_objects:
+                    self.scene_objects[obj]["type"] = obj.split("-")[0]
                     self.scene_objects[obj]["seen"] = True
                     self.scene_objects[obj]["inReceptacle"] = receptacle
-                elif obj.split("-")[0] in self.revevant_task_object_type:
-                    self.scene_objects[obj] = {
-                        "type": "object",
-                        "relevant": True,
-                        "seen": True,
-                        "inReceptacle": receptacle,
-                    }
 
         return scene_changed
 
     def get_plan(self) -> list[str]:
-        problem = self.get_pddl_problem()
-        return call_lakpt_solver(problem, solver="bfs_f", logger=self.logger)
+        problems = self.get_pddl_problem()
+        plans = []
+        for problem in problems:
+            plan = call_lakpt_solver(problem, solver="bfs_f", logger=self.logger)
+            # validate plan
+            if len(plan) > 0:
+                plans.append(plan)
+
+        # greedy selection strategy
+        return min(plans, key=len)
 
     def take_action(self, last_observation="") -> str:
         # sometimes move action doesn't trigger observation (e.g. if already close)
@@ -763,33 +671,3 @@ if __name__ == "__main__":
             "w",
         ) as f:
             json.dump(results, f)
-
-
-# Alternate way of processing observations using LLM (not used because slower/costly)
-# def process_obs(obs: str) -> dict:
-#     prompt = """Convert each observation into a json dict, such that it can be parsed with the python json.loads function. If a receptacle is closed, return an empty dictionary.
-# On the sinkbasin 1, you see a dishsponge 2, a spatula 3, and a tomato 1.
-# {"sinkbasin-1": ["dishsponge-2","spatula-3", "tomato-1"]}
-# You open the cabinet 4. The cabinet 4 is open. In it, you see nothing.
-# {"cabinet-4": []}
-# You open the fridge 1. The fridge 1 is open. In it, you see a apple 2, a egg 3, a egg 2, and a pan 2.
-# {"fridge-1": [ "apple-2", "egg-3", "egg-2", "pan-2"]}
-# On the garbagecan 1, you see a bread 3.
-# {"garbagecan-1": ["bread-3"]}
-# The microwave 1 is closed.
-# {}
-# On the countertop 1, you see a apple 1, a bread 2, a butterknife 1, a mug 2, a potato 2, and a soapbottle 2.
-# {"countertop-1": ["apple-1", "bread-2", "butterknife-1", "mug-2", "potato-2", "soapbottle-2"]}
-# On the coffeemachine 1, you see a mug 1.
-# {"coffeemachine-1": ["mug-1"]}
-# The drawer 2 is closed.
-# {}
-# """
-#     prompt += f"{obs}\n"
-#     out = llm(prompt, stop=["\n"])
-#     try:
-#         out = json.loads(out)
-#     except json.decoder.JSONDecodeError:
-#         print(f"Could not convert observation {obs} into dict from {out}.")
-#         out = {}
-#     return out
